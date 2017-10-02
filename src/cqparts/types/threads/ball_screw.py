e7a7d96bb0f6f0b5b91df5c475d1e4609bf29b64
Peter Boin <Peter.Boin@gmail.com>
2017-10-03 01:45:26 +1100

    profile z=0 at ball's center

--- src/cqparts/types/threads/ball_screw.py
+++ src/cqparts/types/threads/ball_screw.py
@@ -6,13 +6,13 @@ class BallScrewThread(Thread):
 
     def build_profile(self):
         profile = cadquery.Workplane("XZ") \
-            .moveTo(self.radius, self.pitch)
+            .moveTo(self.radius, self.pitch - self.ball_radius)
         # cylindrical face
         if (2 * self.ball_radius) < self.pitch:
-            profile = profile.lineTo(self.radius, 2 * self.ball_radius)
+            profile = profile.lineTo(self.radius, self.ball_radius)
         # rail for balls
         profile = profile.threePointArc(
-                (self.radius - self.ball_radius, self.ball_radius),
-                (self.radius, 0)
+                (self.radius - self.ball_radius, 0),
+                (self.radius, -self.ball_radius)
             )
         return profile.wire()
