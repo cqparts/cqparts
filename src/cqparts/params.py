4a512a1d73514448eb866aa9fb8726feb3d426b9
Peter Boin <Peter.Boin@gmail.com>
2017-11-15 17:23:48 +1100

    ok, I have no idea what's happening.

--- src/cqparts/params.py
+++ src/cqparts/params.py
@@ -5,7 +5,7 @@ from .errors import ParameterError
 import logging
 log = logging.getLogger(__name__)
 
-class ParametricOb ject(object):
+class ParametricObject(object):
     """
     Parametric objects may be defined like so:
 
