4645819a5e1af5ed731e8108cb56f49158a78056
Peter Boin <Peter.Boin@gmail.com>
2017-11-19 22:50:51 +1100

    force build API docs as well

--- docs/build-all.sh
+++ docs/build-all.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+# Force build (delete build directory)
 #rm -r _build/*
-#make html
 
+# ----- Build API sphinx indexes
+api/build.sh -f
+
+# ----- Build html from sphinx indexes
 SPHINXOPTS="-a -E"
 SPHINXBUILD=sphinx-build
 SOURCEDIR=.
 BUILDDIR=_build
 
+#make html
 ${SPHINXBUILD} -M html ${SOURCEDIR} ${BUILDDIR} ${SPHINXOPTS}
