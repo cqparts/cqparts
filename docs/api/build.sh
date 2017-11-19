f48b5a886b693c3858d0163faa7b4603ec1e5364
Peter Boin <Peter.Boin@gmail.com>
2017-11-19 22:50:24 +1100

    can be run remotely and ref the same dir

--- docs/api/build.sh
+++ docs/api/build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-sphinx-apidoc -o . ../../src/cqparts
+# This directory (the folder this script is in)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sphinx-apidoc $* -o ${DIR} ${DIR}/../../src/cqparts
