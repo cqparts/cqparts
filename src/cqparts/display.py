080aaf852b6c44a9f13eff293bc3ac2279c11d9a
Peter Boin <Peter.Boin@gmail.com>
2017-11-27 10:53:20 +1100

    initial concept for display (very basic)

--- src/cqparts/display.py
+++ src/cqparts/display.py
@@ -1,2 +1,10 @@
 
+from .part import Part, Assembly
 from Helpers import show
+
+def display(obj):
+    if isinstance(obj, Part):
+        show(obj.object)
+    elif isinstance(obj, Assembly):
+        for component in obj.components:
+            display(component)
