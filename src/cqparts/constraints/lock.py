817f94222c301402104b8e6ec6ba5276e6f06594
Peter Boin <Peter.Boin@gmail.com>
2017-11-27 16:31:42 +1100

    added RelativeLockConstraint

--- src/cqparts/constraints/lock.py
+++ src/cqparts/constraints/lock.py
@@ -19,3 +19,30 @@ class LockConstraint(Constraint):
         """
         self.component = component
         self.mate = mate
+
+
+class RelativeLockConstraint(Constraint):
+    """
+    Locks an object to a coordinate system offset relative to a part.
+
+    To successfully determine the component's location, the relative component
+    must be solvable.
+
+    .. note::
+        An :class:`Assembly <cqparts.part.Assembly>` **cannot** solely rely
+        on relative locks to place its components.
+        This is because every component will be waiting for another component
+        to be placed, a circular problem.
+    """
+    def __init__(self, component, mate, relative_to):
+        """
+        :param component: component being constrained
+        :type component: :class:`Component <cqparts.part.Component>`
+        :param mate: mate to lock component's coordinate system to
+        :type mate: :class:`Mate <cqparts.constraints.base.Mate>`
+        :param relative_to: ``mate`` is set in this component's coordinate system
+        :type relative_to: :class:`Component <cqparts.part.Component>`
+        """
+        self.component = component
+        self.mate = mate
+        self.relative_to = relative_to
