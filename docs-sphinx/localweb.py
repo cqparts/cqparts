4623e84cdbe5e70c1df6b581dd9c05877e217b06
Peter Boin <Peter.Boin@gmail.com>
2017-12-19 17:33:50 +1100

    threaded web-service (docs dev only)

--- docs-sphinx/localweb.py
+++ docs-sphinx/localweb.py
@@ -1,18 +1,35 @@
 # run using:
 #   ./go.sh web
 
+import os
+import time
 import SimpleHTTPServer
 import SocketServer
+import threading
 
 PORT = 9040
+SocketServer.TCPServer.allow_reuse_address = True
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+os.chdir('./_build/html/')
+httpd = SocketServer.ThreadingTCPServer(("", PORT), Handler)
 
-print("serving: http://localhost:%i/_build/html/" % PORT)
+
+print("serving: http://localhost:%i/" % PORT)
 try:
-    httpd.serve_forever()
+    server_thread = threading.Thread(target=httpd.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+
+    while True:  # wait for Ctrl+C
+        time.sleep(0.5)
+
 except KeyboardInterrupt:
+    print("\n[keyboard interrupt]")
+
+finally:
     httpd.shutdown()
-    print("    [service shutdown]")
+    httpd.server_close()
+    server_thread.join()
+    print("[http shutdown successfully]")
