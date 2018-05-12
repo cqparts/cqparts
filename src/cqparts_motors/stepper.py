4f7a0cc44ec2d30dc7355a2a173eb7b03b50e57a
Simon Kirkby <obeygiantrobot@gmail.com>
2018-05-12 11:23:48 +0000

    Changed height to length , some NEMA sizes
    Was using height parameter , all the doc I could find refers to length.
    
    Generate various NEMA sizes, can generate all of the for catalogue.

--- src/cqparts_motors/stepper.py
+++ src/cqparts_motors/stepper.py
@@ -5,7 +5,6 @@
 # stepper motor generic
 
 # TODO 
-# a collection to pull out NEMA## sizes.
 # need a cutout for mounting
 # even 4 fasteners so it auto mounts to whatever it is parented to.
 
@@ -23,12 +22,12 @@ import shaft
 class _EndCap(cqparts.Part):
     # Parameters
     width = PositiveFloat(42.3, doc="Motor Size")
-    height = PositiveFloat(10, doc="End height")
+    length = PositiveFloat(10, doc="End length")
     cham = PositiveFloat(3, doc="chamfer")
 
     def make(self):
         base = cq.Workplane("XY")\
-            .box(self.width, self.width, self.height)\
+            .box(self.width, self.width, self.length)\
             .edges("|Z")\
             .chamfer(self.cham)
         return base
@@ -36,7 +35,7 @@ class _EndCap(cqparts.Part):
     @property
     def mate_top(self):
         return Mate(self, CoordSystem(
-            origin=(0, 0, -self.height/2),
+            origin=(0, 0, -self.length/2),
             xDir=(0, 1, 0),
             normal=(0, 0, -1)
             ))
@@ -44,7 +43,7 @@ class _EndCap(cqparts.Part):
     @property
     def mate_bottom(self):
         return Mate(self, CoordSystem(
-            origin=(0, 0, -self.height/2),
+            origin=(0, 0, -self.length/2),
             xDir=(0, 1, 0),
             normal=(0, 0, 1)
             ))
@@ -53,14 +52,14 @@ class _EndCap(cqparts.Part):
 class _Stator(cqparts.Part):
     # Parameters
     width = PositiveFloat(40.0, doc="Motor Size")
-    height = PositiveFloat(20, doc="stator height")
+    length = PositiveFloat(20, doc="stator length")
     cham = PositiveFloat(3, doc="chamfer")
 
     _render = render_props(color=(50, 50, 50))
 
     def make(self):
         base = cq.Workplane("XY")\
-            .box(self.width, self.width, self.height)\
+            .box(self.width, self.width, self.length)\
             .edges("|Z")\
             .chamfer(self.cham)
         return base
@@ -68,7 +67,7 @@ class _Stator(cqparts.Part):
     @property
     def mate_top(self):
         return Mate(self, CoordSystem(
-            origin=(0, 0, self.height/2),
+            origin=(0, 0, self.length/2),
             xDir=(0, 1, 0),
             normal=(0, 0, 1)
             ))
@@ -76,7 +75,7 @@ class _Stator(cqparts.Part):
     @property
     def mate_bottom(self):
         return Mate(self, CoordSystem(
-            origin=(0, 0, -self.height/2),
+            origin=(0, 0, -self.length/2),
             xDir=(0, 1, 0),
             normal=(0, 0, 1)
             ))
@@ -86,7 +85,7 @@ class _StepperMount(_EndCap):
     spacing = PositiveFloat(31, doc="hole spacing")
     hole_size = PositiveFloat(3, doc="hole size")
     boss = PositiveFloat(22, doc="boss size")
-    boss_height = PositiveFloat(2, doc="boss_height")
+    boss_length = PositiveFloat(2, doc="boss_length")
 
     def make(self):
         obj = super(_StepperMount, self).make()
@@ -95,7 +94,7 @@ class _StepperMount(_EndCap):
             .vertices() \
             .hole(self.hole_size)
         obj.faces(">Z").workplane()\
-            .circle(self.boss/2).extrude(2)
+            .circle(self.boss/2).extrude(self.boss_length)
         return obj
 
 
@@ -115,30 +114,35 @@ class _Back(_EndCap):
 class Stepper(cqparts.Assembly):
 
     # Shaft type
-    shaft_type = shaft.Shaft 
+    shaft_type = shaft.Shaft
 
     width = PositiveFloat(42.3)
-    height = PositiveFloat(50)
+    length = PositiveFloat(50)
     hole_spacing = PositiveFloat(31.0)
     hole_size = PositiveFloat(3)
     boss_size = PositiveFloat(22)
-    boss_height = PositiveFloat(2)
+    boss_length = PositiveFloat(2)
 
     shaft_diam = PositiveFloat(5)
     shaft_length = PositiveFloat(24)
 
     def make_components(self):
-        sec = self.height / 6
+        sec = self.length / 6
         return {
             'topcap': _StepperMount(
                 width=self.width,
-                height=sec,
+                length=sec,
                 spacing=self.hole_spacing,
                 hole_size=self.hole_size,
                 boss=self.boss_size
             ),
-            'stator': _Stator(width=self.width-3, height=sec*4),
-            'botcap': _Back(width=self.width, height=sec),
+            'stator': _Stator(width=self.width-3, length=sec*4),
+            'botcap': _Back(
+                width=self.width,
+                length=sec,
+                spacing=self.hole_spacing,
+                hole_size=self.hole_size,
+            ),
             'shaft': self.shaft_type(
                 length=self.shaft_length,
                 diam=self.shaft_diam)
@@ -158,7 +162,7 @@ class Stepper(cqparts.Assembly):
             Fixed(
                 self.components['shaft'].mate_origin,
                 CoordSystem(
-                    (0, 0, self.height/8-self.boss_height),
+                    (0, 0, self.length/8-self.boss_length),
                     (1, 0, 0),
                     (0, 0, 1)
                 )
@@ -173,4 +177,45 @@ class Stepper(cqparts.Assembly):
 
     def make_alterations(self):
         self.apply_cutout()
+
+# Sized motors , build into collection
+# http://www.osmtec.com/stepper_motors.htm is a good reference
+# sizes 8, 11, 14, 16, 17, 23, 24, 34, 42
+# s = Stepper()
+# s.serialize_parameters().keys()
+#['shaft_length', 'hole_spacing', 'hole_size', 'length', 'width', 'boss_size', 'shaft_diam', 'boss_length']
+
+_NemaSizes = {
+    8 :  [10.0, 15.4, 2.0, 28.0, 20.3, 16.0, 4.0, 1.5],
+    11 : [20.0, 23.0, 2.5, 31.5, 28.2, 22.0, 4.0, 2.0],
+    14 : [24.0, 26.0, 3.0, 28.0, 35.2, 22.0, 5.0, 2.0],
+    17 : [24.0, 31.0, 3.0, 50.0, 42.0, 22.0, 5.0, 2.0],
+    23 : [21.0, 47.0, 5.0, 56.0, 57.0, 38.0, 6.35, 1.6],
+    # finish sizes
+}
+
+def NEMA(size):
+    if _NemaSizes.has_key(size):
+        return _GetNEMASize(_NemaSizes[size])
+    else:
+        raise 
+
+def _GetNEMASize(v):
+    s = Stepper(
+        shaft_length=v[0],
+        hole_spacing=v[1],
+        hole_size=v[2],
+        length=v[3],
+        width=v[4],
+        boss_size=v[5],
+        shaft_diam=v[6],
+        boss_length=v[7]
+    )
+    return s
+
+def _GenSteppers():
+    sizes = {}
+    for size,values in _NemaSizes.items():
+        sizes[size] = _GetNEMASize(values)
+    return sizes
 
