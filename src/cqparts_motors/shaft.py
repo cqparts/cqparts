9a3c66cdd7276b94a50ba56cde07cfd440f50dc2
Simon Kirkby <obeygiantrobot@gmail.com>
2018-05-12 00:51:28 +0000

    shaft cleanup

--- src/cqparts_motors/shaft.py
+++ src/cqparts_motors/shaft.py
@@ -1,12 +1,21 @@
+# cp parts
+# base shaft collection
+
+# 2018 Simon Kirkby obeygiantrobot@gmail.com
+
+# TODO
+# need tip , base and offset mate points
+# maybe shaft needs to go into it's own module
+#
+# there are lots of types of shafts and extras
+# need a clean way to build shafts
 
 import cadquery as cq
 
 import cqparts
-from cqparts.params import *
-from cqparts.display import render_props, display
-from cqparts.constraint import Fixed, Coincident
+from cqparts.params import PositiveFloat
+from cqparts.display import render_props
 from cqparts.constraint import Mate
-from cqparts.utils.geometry import CoordSystem
 
 # base shaft type
 class Shaft(cqparts.Part):
