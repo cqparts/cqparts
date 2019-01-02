0718808576f30e9ebc5bb76c897b8d2b81cb8457
Peter Boin <Peter.Boin@gmail.com>
2019-01-03 00:57:47 +1100

    matrix evaluation by index

--- tests/t_cqparts/test_coordsystem.py
+++ tests/t_cqparts/test_coordsystem.py
@@ -18,10 +18,10 @@ class CoordSystemTests(CQPartsTest):
         return [
             round(v, digits)
             for v in [
-                m.A11, m.A12, m.A13, m.A14,
-                m.A21, m.A22, m.A23, m.A24,
-                m.A31, m.A32, m.A33, m.A34,
-                m.A41, m.A42, m.A43, m.A44
+                m[0,0], m[0,1], m[0,2], m[0,3],
+                m[1,0], m[1,1], m[1,2], m[1,3],
+                m[2,0], m[2,1], m[2,2], m[2,3],
+                m[3,0], m[3,1], m[3,2], m[3,3],
             ]
         ]
 
