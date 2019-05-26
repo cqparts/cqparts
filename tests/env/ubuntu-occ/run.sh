78acd3eb921de96aae91d8fab3fa6cdc90bab2b6
Peter Boin <Peter.Boin@gmail.com>
2019-05-27 07:38:14 +1000

    easy access to "core" tests

--- tests/env/ubuntu-occ/run.sh
+++ tests/env/ubuntu-occ/run.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 source ./common-vars.sh
 
+function run_container() {
+    docker run --rm --volume ${CQPARTS_ROOT}:/code -it ${IMAGE} "$@"
+}
+
 case "$1" in
     "")
         # Run tests in interactive mode
-        docker run --rm --volume ${CQPARTS_ROOT}:/code -it ${IMAGE}
+        run_container
         ;;
     non-interactive)
         # Run tests (non-interactive; for continuous integration)
         docker run --rm --volume ${CQPARTS_ROOT}:/code ${IMAGE}
         ;;
+    core)
+        # Only run core t_cqparts tests
+        run_container python3 runtests.py --module cqparts
+        ;;
     *)
-        docker run --rm --volume ${CQPARTS_ROOT}:/code -it ${IMAGE} "${@:1}"
+        run_container "${@:1}"
         ;;
 esac
